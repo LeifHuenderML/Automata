070e2b5adf962797a804c543b87958e891b0e600
Leif Huender <leifhuenderai@gmail.com>
2024-02-05 09:25:11 -0800

    resfactoring engine

--- rlEngine/environment.cpp
+++ rlEngine/environment.cpp
@@ -27,11 +27,6 @@
 
 #include "environment.hpp"
 
-void Env::initTensors(const int& envSize){ cells = torch::ones({1, envSize, envSize}); }
-
-torch::Tensor Env::updateEnv(const torch::Tensor& actions){
-    torch::Tensor newCells = cells.clone(); 
-    auto accessor = cells.accessor<bool, 2>();
 
     for(int row = 1; row < accessor.size(0) - 1; ++row){
         for(int col = 1; col < accessor.size(1) - 1; ++col){
@@ -63,49 +58,3 @@ torch::Tensor Env::updateEnv(const torch::Tensor& actions){
 
     return newCells;
 }
-
-float Env::calcReward(torch::Tensor determinedActions){
-    int currentLivingCells = 0;
-    int previousLivingCells = 0;
-    
-    auto newAccessor = determinedActions.accessor<bool, 2>();
-    auto oldAccessor = cells.accessor<bool, 2>();
-
-    for(int row = 0; row < newAccessor.size(0); ++row){
-        for(int col = 0; col < newAccessor.size(1); ++col){
-            if(newAccessor[row][col]){
-                currentLivingCells++;
-            }
-            if(oldAccessor[row][col]){
-                previousLivingCells++;
-            }
-        }
-    }
-    totalLiving = currentLivingCells;
-    int increase = currentLivingCells - previousLivingCells;
-    //calculates the reward to be the increase in cell count divided by the size of the envionmrent
-    return static_cast<float>(increase) / envSize * envSize; 
-}
-
-bool Env::ifDone(){ return (totalLiving == envSize * envSize || currSteps == maxSteps); }
-
-
-void Env::step(const torch::Tensor& actions, float& reward, bool& done) {
-
-    torch::Tensor determinedActions = updateEnv(actions);;
-
-    reward = calcReward(determinedActions);
-
-    done = ifDone();
-
-    ++currSteps;
-
-    cells = determinedActions;
-}
-
-torch::Tensor Env::reset(){
-    cells = torch::zeros({envSize, envSize}, torch::TensorOptions().dtype(torch::kBool)); 
-    totalLiving = 0;
-    currSteps = 0;
-    return cells;
-    }
--- rlEngine/model.hpp
+++ rlEngine/model.hpp
@@ -28,20 +28,41 @@
 #include <torch/torch.h>
 
 
-struct PolicyNetwork : torch::nn::Module {
-    PolicyNetwork() {
-        conv1 = register_module("conv1", torch::nn::Conv2d(1, 8, 3, 1, 1));
-        conv2 = register_module("conv2", torch::nn::Conv2d(8, 16, 3, 1, 1));
-        conv3 = register_module("conv3", torch::nn::Conv2d(16, 1, 3, 1, 1));
+#include <torch/torch.h>
+
+struct PolicyNetworkImpl : torch::nn::Module{
+    //default of 200, meaning 100 x y coordinates;
+    PolicyNetworkImpl(int64_t output_size = 200){
+        //declaring 4 convolutional layers
+        conv1 = register_module("conv1", torch::nn::Conv2d(torch::nn::Conv2dOptions(1, 16, 3).stride(2).padding(1)));
+        conv2 = register_module("conv2", torch::nn::Conv2d(torch::nn::Conv2dOptions(16, 32, 3).stride(2).padding(1)));
+        conv3 = register_module("conv3", torch::nn::Conv2d(torch::nn::Conv2dOptions(32, 64, 3).stride(2).padding(1)));
+        conv4 = register_module("conv4", torch::nn::Conv2d(torch::nn::Conv2dOptions(64, 128, 3).stride(2).padding(1)));
+        //declaring 3 fully connected dense layers
+        fc1 = register_module("fc1", torch::nn::Linear(128 * 64 * 64, 1024));
+        fc2 = register_module("fc2", torch::nn::Linear(1024, 256));
+        fc3 = register_module("fc3", torch::nn::Linear(256, output_size));
     }
-
-    torch::Tensor forward(torch::Tensor x) {
-
+    //passes the tensor through each of the layres apllying the ReLU asctivation function to each layer
+    torch::Tensor forward(torch::Tensor x){
         x = torch::relu(conv1->forward(x));
         x = torch::relu(conv2->forward(x));
-        x = torch::sigmoid(conv3->forward(x));
+        x = torch::relu(conv3->forward(x));
+        x = torch::relu(conv4->forward(x));
+        //flattens tensor to pass to the fully connected layers
+        x = x.view({x.size(0), -1});   
+        //passes the tensor through each of the layres apllying the ReLU asctivation function to each layer
+        x = torch::relu(fc1->forward(x));
+        x = torch::relu(fc2->forward(x));
+        x = fc3->forward(x);
+        //returns a output of output_size coordinates
         return x;
     }
-
-    torch::nn::Conv2d conv1, conv2, conv3;
+    //defines each layer to be of its respective class, and initializes them all with null ponters
+    torch::nn::Conv2d conv1{nullptr}, conv2{nullptr}, conv3{nullptr}, conv4{nullptr};
+    torch::nn::Linear fc1{nullptr}, fc2{nullptr}, fc3{nullptr};
 };
+
+//wrapper for shared pointer creation
+TORCH_MODULE(PolicyNetwork);
+
