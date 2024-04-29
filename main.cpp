33e32a1ff5121deb8e9e8b4b382bc42ae93c4564
Leif Huender <leifhuenderai@gmail.com>
2024-04-29 10:42:12 -0700

    added music

--- main.cpp
+++ main.cpp
@@ -7,9 +7,9 @@
 
 void playMusic(sf::SoundBuffer& buffer);
 
-int main() {
+int main(){
     sf::SoundBuffer buffer;
-    if (!buffer.loadFromFile("music/lofi.wav")) {
+    if(!buffer.loadFromFile("music/lofi.wav")){
         std::cerr << "Could not load music file." << std::endl;
         return -1;  
     }
@@ -26,11 +26,11 @@ int main() {
     std::string str2 = "High";
     GameLoop game;
     std::cout << player << std::endl;
-    if (mode == 1) {
+    if(mode == 1){
         game.gameLoop(str, player);
-    } else if (mode == 2) {
+    } else if(mode == 2){
         game.gameLoop(str1, player);
-    } else if (mode == 3) {
+    } else if(mode == 3){
         game.gameLoop(str2, player);
     }
 
@@ -39,13 +39,13 @@ int main() {
     return 0;
 }
 
-void playMusic(sf::SoundBuffer& buffer) {
+void playMusic(sf::SoundBuffer& buffer){
     sf::Sound sound;
     sound.setBuffer(buffer);
     sound.setLoop(true);
     sound.play();
 
-    while (sound.getStatus() == sf::Sound::Playing) {
+    while(sound.getStatus() == sf::Sound::Playing){
         std::this_thread::sleep_for(std::chrono::seconds(1));
     }
 }
